421b0842bc430c1a163a3603e62aec6b3d1a2529
Thicianny <thicianny2@gmail.com>
2025-09-13 15:04:07 -0300

    fix: about link

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <section :id="aboutData.id" :class="['py-16 sm:py-20', aboutData.background]">
+  <section id="sobre" :id="aboutData.id" :class="['py-16 sm:py-20', aboutData.background]">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-12 items-center">
         <div>
